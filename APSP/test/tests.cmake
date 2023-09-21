db308f02514a5b35f82773309bbc61dbd08c1a81
Eigensystem <eigensystem1318@gmail.com>
2023-09-22 01:57:25 +0800

    feat: ctest available

---
+++ APSP/test/tests.cmake
@@ -0,0 +1,9 @@
+getFiles(Inputs "/tmp/dataset/APSP" ".*raw.*")
+getCheckFiles(CheckFiles Inputs)
+
+foreach(TestName TestExe IN ZIP_LISTS SubDirs Test_Executables)
+    foreach(Input Check IN ZIP_LISTS Inputs CheckFiles)
+        getCaseSize(Size ${Input})
+        add_test(NAME "APSP_${TestName}_${Size}" COMMAND ${TestExe} ${Input} ${Check})
+    endforeach()
+endforeach()
